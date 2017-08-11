9a1425e1b45971404a59147a2e7701df1f32a5a9
MrPingu <luizrodrigo40@gmail.com>
2017-08-10 23:36:35 -0300

    Converted Text API enums

---
+++ src/main/java/br/com/dusty/dkits/util/text/TextStyle.kt
@@ -0,0 +1,26 @@
+package br.com.dusty.dkits.util.text
+
+import org.bukkit.ChatColor
+
+/**
+ * Define estilos a serem usados em um [Text].
+ */
+enum class TextStyle private constructor(internal var code: Int, chatColor: ChatColor) {
+
+    MAGIC(0x10, ChatColor.MAGIC),
+    BOLD(0x11, ChatColor.BOLD),
+    STRIKETHROUGH(0x12, ChatColor.STRIKETHROUGH),
+    UNDERLINE(0x13, ChatColor.UNDERLINE),
+    ITALIC(0x14, ChatColor.ITALIC),
+    RESET(0x15, ChatColor.RESET);
+
+    internal var string: String
+
+    init {
+        string = chatColor.toString()
+    }
+
+    override fun toString(): String {
+        return string
+    }
+}
